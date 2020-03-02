1687c0a0947315d671d6ee0edceb345718e78277
camiaubry <aubry.camille35@gmail.com>
2020-03-02 16:26:43 +0100

    Added test JUnit

--- src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
+++ src/test/java/com/ipiecoles/java/java350/model/EmployeTest.java
@@ -3,6 +3,8 @@ package com.ipiecoles.java.java350.model;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
 
@@ -33,4 +35,24 @@ public class EmployeTest {
         //Then
         Assertions.assertEquals(0, anneeAnciennete.intValue());
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 'T12345', 0, 1.0, 1000.0",
+            "1, 'T12345', 2, 0.5, 600.0",
+            "1, 'T12345', 2, 1.0, 1200.0",
+            "2, 'M12345', 8, 1.0, 2500.0"
+    })
+    public void getPrimeAnnuelle(Integer performance, String matricule, Long nbYearsAnciennete, Double tempsPartiel, Double primeAnnuelle){
+        //Given
+        Employe employe = new Employe("Doe", "John", matricule, LocalDate.now().minusYears(nbYearsAnciennete), Entreprise.SALAIRE_BASE, performance, tempsPartiel);
+
+        //When
+        Double prime = employe.getPrimeAnnuelle();
+
+        //Then
+        Assertions.assertEquals(primeAnnuelle, prime);
+
+    }
+
 }
